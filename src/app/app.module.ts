aef2bf3ec4e1854371cd4397b7679dc5b2db9936
MikeZarpa <meschneider2@gmail.com>
2022-07-21 17:48:44 -0300

    Maquetamos el portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './mis-componentes/header/header.component';
+import { AcercaDeComponent } from './mis-componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './mis-componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './mis-componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './mis-componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './mis-componentes/proyectos/proyectos.component';
+import { LoginComponent } from './mis-componentes/login/login.component';
+import { ExpItemComponent } from './mis-componentes/experiencia/exp-item/exp-item.component';
+import { EduItemComponent } from './mis-componentes/educacion/edu-item/edu-item.component';
+import { SkillItemComponent } from './mis-componentes/habilidades/skill-item/skill-item.component';
+import { ProyectoItemComponent } from './mis-componentes/proyectos/proyecto-item/proyecto-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    LoginComponent,
+    ExpItemComponent,
+    EduItemComponent,
+    SkillItemComponent,
+    ProyectoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mis-componentes/educacion/edu-item/edu-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edu-item',
+  templateUrl: './edu-item.component.html',
+  styleUrls: ['./edu-item.component.css']
+})
+export class EduItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
