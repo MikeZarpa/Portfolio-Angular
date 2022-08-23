fdf7bd1bb68fd86a4ac373575f7a6c442ebc3ab3
MikeZarpa <meschneider2@gmail.com>
2022-08-23 20:07:32 -0300

    Agregado FireBase al proyecto. Se agregó un servicio para el manejo de la carga de imagenes. Se creó una clase nueva para dar funciones para la carga de imagenes (EdicionItemConImagen). Implementada la clase en el complemento item de educación. Agregado un componente para la edición de imagenes (img-edit-btns) comprende el inputFile, y botones cancelar y cargar.

---
+++ src/app/mis-componentes/edicion/img-edit-btns/img-edit-btns.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-img-edit-btns',
+  templateUrl: './img-edit-btns.component.html',
+  styleUrls: ['./img-edit-btns.component.css']
+})
+export class ImgEditBtnsComponent implements OnInit {
+
+  @Input() mostrarControles=false;
+  @Input() ocultarBotones = false;
+  @Output() inputChange = new EventEmitter<any>()
+  @Output() cargar = new EventEmitter<void>()
+  @Output() cancelar = new EventEmitter<void>()
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event:any){
+    this.inputChange.emit(event);
+  }
+  onCargar(){
+    this.cargar.emit();
+  }
+  onCancelar(){
+    this.cancelar.emit();
+  }
+
+}
---
+++ src/app/mis-servicios/ImageStorageService/image-storage.service.ts
@@ -0,0 +1,111 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import { environment } from 'src/environments/environment';
+
+const FIREBASE_DATA = {
+  apiKey: "AIzaSyBpKKepX5Iu7I-ucI6bjLJNzD2odIxEwEc",
+  authDomain: "proyecto-de-imagenes.firebaseapp.com",
+  projectId: "proyecto-de-imagenes",
+  storageBucket: "proyecto-de-imagenes.appspot.com",
+  messagingSenderId: "1090566699413",
+  appId: "1:1090566699413:web:bb216b9079bb9317f29e9c",
+  measurementId: "G-85XF7XGB4Z"
+}
+
+firebase.initializeApp(FIREBASE_DATA);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageStorageService {
+
+  storareRef = firebase.app().storage().ref();
+
+  constructor() {
+  }
+
+  private async subirImagen(nombreImg: string, imgBase64: any):Promise<string|null>{
+    try {
+      const respuesta = await this.storareRef.child("/ImagenesPortfolio/" + nombreImg).putString(imgBase64, 'data_url');      
+      console.log(respuesta);
+      //Esperamos que nos devuelva una dirección de descarga (es una promesa).
+      return await respuesta.ref.getDownloadURL();
+    } catch (err) {
+      console.log("Error al subir la imagen");      
+      console.log(err);
+      return null;
+    }    
+  }
+
+  subirImagen2(nombreCarpeta:string,nombreImg: string, imgBase64: any):void{
+    try {
+      //Con esto ya se sube
+      const respuesta =  this.storareRef.child("/ImagenesPortfolio/"+nombreCarpeta+"/" + nombreImg).putString(imgBase64, 'data_url');
+    } catch (err) {
+      console.log("Error al subir la imagen");
+    }    
+  }
+
+  cargarImagen(fileInputEvent: any,callback:Function) {
+    let imgFile = fileInputEvent.target.files;
+    let reader = new FileReader();
+    let nombreImg = 'img';
+
+    reader.readAsDataURL(imgFile[0]);
+    reader.onloadend = () => {
+      //console.log(reader.result);
+      return this.subirImagen(nombreImg + '_' + Date.now(), reader.result)
+    };
+  }
+  async cargaImagen(
+    fileInputEvent: any,
+    callback:(a:string)=>void,
+    callbackError:()=>void)
+    {
+    let imgFile = fileInputEvent.target.files[0];
+    let fileReader = new FileReader();
+    let nombreImg = 'img';
+    fileReader.onloadend = async () => {
+      await this.subirImagen(nombreImg + '_' + Date.now(), fileReader.result).then(
+        res =>{
+          if(res) callback(res);
+          else callbackError();
+        }
+      );
+    }
+    fileReader.readAsDataURL(imgFile);    
+  }
+  async cargaImagenDesdeFile(
+    file: any,
+    callback:(a:string)=>void,
+    callbackError:()=>void)
+    {
+      let imgFile = file;
+      let fileReader = new FileReader();
+      let nombreImg = 'img';
+      fileReader.onloadend = async () => {
+        return await this.subirImagen(nombreImg + '_' + Date.now(), fileReader.result).then(
+          url =>{
+            if(url) callback(url);
+            else callbackError();
+          }
+        );
+      }
+      fileReader.readAsDataURL(imgFile);
+  }
+
+  async readFile(file: any):Promise<string>{
+    return await new Promise((res, rej) => {
+      var fr = new FileReader();  
+      let nombreImg = 'img';
+
+      fr.onload = async () => {
+        await this.subirImagen(nombreImg + '_' + Date.now(), fr.result).then(resp=>{
+          res(resp??"")
+        }).catch(err=> rej(err));
+      };
+      fr.readAsDataURL(file);
+    });
+  }
+}
