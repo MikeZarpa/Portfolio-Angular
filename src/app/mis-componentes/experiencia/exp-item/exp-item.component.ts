aed84b923cf09ee1b522a55e437a7207af9cbd2d
MikeZarpa <meschneider2@gmail.com>
2022-08-23 20:24:35 -0300

    Implementado edición de imagenes en ExpItem

--- src/app/mis-componentes/experiencia/exp-item/exp-item.component.ts
+++ src/app/mis-componentes/experiencia/exp-item/exp-item.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Experiencia } from 'src/app/mis-classes/models/experiencia';
-import { EdicionItem } from 'src/app/mis-classes/paraEdicion/edicion-item';
+import { EdicionItemConImagen } from 'src/app/mis-classes/paraEdicion/edicion-item-con-imagen';
+import { ImageStorageService } from 'src/app/mis-servicios/ImageStorageService/image-storage.service';
 
 @Component({
   selector: 'app-exp-item',
   templateUrl: './exp-item.component.html',
   styleUrls: ['./exp-item.component.css']
 })
-export class ExpItemComponent extends EdicionItem<Experiencia> implements OnInit {
-  constructor() {
-    super()
+export class ExpItemComponent extends EdicionItemConImagen<Experiencia> implements OnInit {
+  constructor(imgStorage:ImageStorageService) {
+    super(imgStorage);
    }
 
   cambiandoLugar(texto:string){
@@ -23,5 +24,10 @@ export class ExpItemComponent extends EdicionItem<Experiencia> implements OnInit
   }
   cambiandoPuesto(texto:string){
     this.item.puestoNombre=texto;
+  }
+
+  override cargarUrlImg(url: string): void {
+    super.cargarUrlImg(url);
+    this.item.img_logo=url;
   }
 }
